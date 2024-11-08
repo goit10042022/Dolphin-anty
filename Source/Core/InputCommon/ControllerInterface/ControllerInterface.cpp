lckybstrd/dolphin-emu2

InputCommon: Add gamepads from GCAdapter code to ControllerInterface.

Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
@@ -8,6 +8,7 @@
 #include "Common/Assert.h"
 #include "Common/Logging/Log.h"
 #include "Core/HW/WiimoteReal/WiimoteReal.h"
+#include "InputCommon/ControllerInterface/WiiUAdapter/WiiUAdapter.h"
 
 #ifdef CIFACE_USE_WIN32
 #include "InputCommon/ControllerInterface/Win32/Win32.h"
@@ -85,6 +86,8 @@ void ControllerInterface::Initialize(const WindowSystemInfo& wsi)
 #ifdef CIFACE_USE_STEAMDECK
   m_input_backends.emplace_back(ciface::SteamDeck::CreateInputBackend(this));
 #endif
+
+  m_input_backends.emplace_back(ciface::WiiUAdapter::CreateInputBackend(this));
 
   // Don't allow backends to add devices before the first RefreshDevices() as they will be cleaned
   // there. Or they'd end up waiting on the devices mutex if populated from another thread.

goit10042022/Dolphin-anty

Source/Core/InputCommon/ControllerInterface/ControllerInterface.cpp
@@ -8,6 +8,7 @@
 #include "Common/Assert.h"
 #include "Common/Logging/Log.h"
 #include "Core/HW/WiimoteReal/WiimoteReal.h"
+#include "InputCommon/ControllerInterface/GCAdapter/GCAdapter.h"
 
 #ifdef CIFACE_USE_WIN32
 #include "InputCommon/ControllerInterface/Win32/Win32.h"
@@ -85,6 +86,8 @@ void ControllerInterface::Initialize(const WindowSystemInfo& wsi)
 #ifdef CIFACE_USE_STEAMDECK
   m_input_backends.emplace_back(ciface::SteamDeck::CreateInputBackend(this));
 #endif
+
+  m_input_backends.emplace_back(ciface::GCAdapter::CreateInputBackend(this));
 
   // Don't allow backends to add devices before the first RefreshDevices() as they will be cleaned
   // there. Or they'd end up waiting on the devices mutex if populated from another thread.