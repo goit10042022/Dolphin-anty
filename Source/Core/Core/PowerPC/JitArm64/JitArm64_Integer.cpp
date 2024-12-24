dolphin-emu/dolphin

Jit: Move addix to ConstantPropagation

Source/Core/Core/PowerPC/JitArm64/JitArm64_Integer.cpp
@@ -221,18 +221,11 @@ void JitArm64::addix(UGeckoInstruction inst)
 
   if (a)
   {
-    if (gpr.IsImm(a))
-    {
-      gpr.SetImmediate(d, gpr.GetImm(a) + imm);
-    }
-    else
-    {
-      gpr.BindToRegister(d, d == a);
+    gpr.BindToRegister(d, d == a);
 
-      ARM64Reg WA = gpr.GetReg();
-      ADDI2R(gpr.R(d), gpr.R(a), imm, WA);
-      gpr.Unlock(WA);
-    }
+    ARM64Reg WA = gpr.GetReg();
+    ADDI2R(gpr.R(d), gpr.R(a), imm, WA);
+    gpr.Unlock(WA);
   }
   else
   {

goit10042022/Dolphin-anty

Source/Core/Core/PowerPC/JitArm64/JitArm64_Integer.cpp
@@ -217,17 +217,10 @@ void JitArm64::addix(UGeckoInstruction inst)
 
   if (a)
   {
-    if (gpr.IsImm(a))
-    {
-      gpr.SetImmediate(d, gpr.GetImm(a) + imm);
-    }
-    else
-    {
-      gpr.BindToRegister(d, d == a);
+    gpr.BindToRegister(d, d == a);
 
-      auto WA = gpr.GetScopedReg();
-      ADDI2R(gpr.R(d), gpr.R(a), imm, WA);
-    }
+    auto WA = gpr.GetScopedReg();
+    ADDI2R(gpr.R(d), gpr.R(a), imm, WA);
   }
   else
   {